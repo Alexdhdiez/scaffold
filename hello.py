f6604022bcb10811d24c82d9a6d128df720831fc
Alejandro <alexdhdiez@gmail.com>
2022-02-07 16:59:34 +0000

    Changed test file

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
-def toyou(x):
-    return "hi %s" %x
-
-def add(x):
-    return x + 1
-
-def substract(x):
-    return x - 1
+def add(x, y):
+    return x + y
 
 #var=
-#result = add(1, 2)
-#print("This is the sum: 1, 2, %s" % result)
+result = add(1, 2)
+print("This is the sum: 1, 2, %s" % result)
--- hello_test.py
+++ hello_test.py
@@ -1,12 +1,4 @@
-from hello import add, toyou, substract
+from hello import add
 
-def setup_function(function):
-    print("Running Setup: %s" % {function.__name__})
-    function.x = 10
-
-def teardown_function(function):
-    print("Running Teardown: %s" % {function.__name__})
-    del function.x
-
-def test_hello_substact():
-    assert substract(test_hello_substact.x)==9
+def test_add():
+    assert add(1,2) == 3
